17e7bda4418a5bae5006a637753950b3271af3ea
BardoftheOzarks <1886@holbertonschool.com>
2021-08-10 20:32:51 -0500

    added player movement

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // reference to player Rigidbody
+    public Rigidbody rb;
+    // Variable for player speed
+    public fload speed = 2000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Forward
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        // Backward
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        // Left
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        // Right
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
